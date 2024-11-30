a1a12132da0b1d8d167079a6e0abdb2defa23ce3
lebam1210 <lebamchoque@gmail.com>
2024-11-29 23:27:54 -0400

    Estructura del proyecto

---
+++ resources/views/categorias/create.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.menu')
+@section('contenido')
+<h2>FORMULARIO DE CATEGORIA</h2>
+@endsection
---
+++ resources/views/categorias/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.menu')
+@section('contenido')
+<h2>CATEGORIA</h2>
+<a href="{{route('categorias.create')}}" class="btn btn-primary">Nueva Categoria</a>
+@endsection
